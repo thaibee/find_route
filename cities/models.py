6625a9c9e17fbe46644625b9af03c20b2068ca93
doctor.madbee@gmail.com <Password@12>
2021-06-15 00:05:46 +0700

    cities_details

--- cities/models.py
+++ cities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Cities(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True, verbose_name='Город')
 
     def __str__(self):
         return self.name
