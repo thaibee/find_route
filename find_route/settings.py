58d90e604ac8db092dfc4a78044e8f61c76c7cf0
magadanthai <doctor.madbee@gmail.com>
2021-06-14 13:12:18 +0700

    change setting for 2 parts

--- find_route/settings.py
+++ find_route/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,17 +70,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'find_route.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
