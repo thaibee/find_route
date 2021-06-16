65b5d890085086246d5743afb151e7552d8850dc
magadanthai <doctor.madbee@gmail.com>
2021-06-16 15:27:34 +0700

    Редактирование и Удаление городов

--- cities/urls.py
+++ cities/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CitiesList, CityDetail, CityAdd
+from .views import CitiesList, CityDetail, CityAdd, CityUpdate, CityDelete
 
-urlpatterns = [
+urlpatterns = (
     path('', CitiesList.as_view(), name='cities_list'),
     path('city/<int:pk>', CityDetail.as_view(), name='city'),
+    path('update/<int:pk>', CityUpdate.as_view(), name='update'),
+    path('delete/<int:pk>', CityDelete.as_view(), name='delete'),
     path('add/', CityAdd.as_view(), name='add'),
-]
+)
--- cities/views.py
+++ cities/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from cities.forms import CitiesForm
 from cities.models import Cities
@@ -23,12 +23,27 @@ class CitiesList(View):
         return render(request, 'cities/cities_list.html', {'model': model, 'form': form})
 
 
-
 class CityDetail(DetailView):
     model = Cities
+
 
 class CityAdd(CreateView):
     model = Cities
     form_class = CitiesForm
     template_name = 'cities/cities_add.html'
     success_url = reverse_lazy('cities_list')
+
+
+class CityUpdate(UpdateView):
+    model = Cities
+    form_class = CitiesForm
+    template_name = 'cities/cities_update.html'
+    success_url = reverse_lazy('cities_list')
+
+
+class CityDelete(DeleteView):
+    model = Cities
+    success_url = reverse_lazy('cities_list')
+
+    def get(self, request, *args, **kwargs):  # удаление без подтверждения.
+        return self.post(self, request, *args, **kwargs)
